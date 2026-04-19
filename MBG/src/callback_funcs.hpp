8a52f365c7d9a099e8cd251ec506aaf26855f97c
jjcarsello <jamesjc4@illinois.edu>
2026-04-18 22:52:55 -0500

    added some more scrollers for the density elements and made sure it only shows on the 1 that actually takes that data

--- MBG/src/callback_funcs.hpp
+++ MBG/src/callback_funcs.hpp
@@ -5,7 +5,7 @@ using namespace glm;
 using namespace MBG;
 
 extern std::vector<std::string> shader_files;
-extern size_t shader_idx;
+extern size_t shader_idx; // Made these extern so I can mess with them in main.cpp for the ImGui widgets
 
 void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
 
